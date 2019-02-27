85c9b283261328b2fe981ccf02487908de48b726
Stanisław Góra <stasgora@gmail.com>
2019-02-27 21:43:31 +0100

    initial commit

---
+++ src/main/java/edu/file/encryption/app/FileEncryptionApp.java
@@ -0,0 +1,23 @@
+package edu.file.encryption.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FileEncryptionApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("/layout.fxml"));
+		primaryStage.setTitle("File Encryption App");
+		primaryStage.setScene(new Scene(root, 1000, 700));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ src/main/java/edu/file/encryption/app/WindowController.java
@@ -0,0 +1,5 @@
+package edu.file.encryption.app;
+
+public class WindowController {
+
+}

c3ea9ae8a15e8fd98d15a790b9680d4b2800f8e1
Stanisław Góra <stasgora@gmail.com>
2019-03-22 21:29:20 +0100

    added basic GUI with file chooser

--- src/main/java/edu/file/encryption/app/FileEncryptionApp.java
+++ src/main/java/edu/file/encryption/app/FileEncryptionApp.java
@@ -9,12 +9,14 @@ import javafx.stage.Stage;
 public class FileEncryptionApp extends Application {
 
 	@Override
-	public void start(Stage primaryStage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("/layout.fxml"));
-		primaryStage.setTitle("File Encryption App");
-		primaryStage.setScene(new Scene(root, 1000, 700));
-		primaryStage.show();
-		// Test
+	public void start(Stage stage) throws Exception{
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/layout.fxml"));
+		Parent root = loader.load();
+		stage.setTitle("File Encryption App");
+		stage.setScene(new Scene(root, 500, 300));
+		stage.show();
+		WindowController controller = loader.getController();
+		controller.init(stage);
 	}
 
 	public static void main(String[] args) {
--- src/main/java/edu/file/encryption/app/WindowController.java
+++ src/main/java/edu/file/encryption/app/WindowController.java
@@ -1,5 +1,35 @@
 package edu.file.encryption.app;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class WindowController {
 
+	public Label inputFileLabel;
+	public TextField outputFileName;
+	public Button sendButton;
+	public ProgressBar sendProgressBar;
+	private Stage stage;
+
+	public void init(Stage stage) {
+		this.stage = stage;
+	}
+
+	public void loadFile(ActionEvent event) {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Choose File");
+		File file = fileChooser.showOpenDialog(stage);
+	}
+
+	public void exit(ActionEvent actionEvent) {
+		Platform.exit();
+	}
 }
